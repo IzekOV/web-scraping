5de2646ee939638722df90c13e995bd3d97b1b46
IzekOV <hocinidjamalunv@gmail.com>
2026-03-15 16:42:51 +0000

    add while loop in books.py so u get to the next page and get books, add .gitignore files, evrything is run

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
-from src.books import get_categories
+from src.books import get_books, get_categories
+import pandas as pd
 
 url = "https://books.toscrape.com/"
 
 categories = get_categories(url)
+
+# Create a DataFrame from the categories list
+df = pd.DataFrame(categories, columns=['Category Name', 'Category URL'])
+#df.to_csv('categories.csv', index=False)
+
+if df.size > 0:
+    print(f"Categories have been saved to categories.csv, total categories: {len(df)}")
+else:
+    print("No categories found.")
+
+books = get_books("https://books.toscrape.com/catalogue/category/books/historical-fiction_4/index.html")
+dfb = pd.DataFrame(books, columns=['Book Title', 'Book URL'])
+dfb.to_csv('books.csv', index=False)
--- src/books.py
+++ src/books.py
@@ -1,4 +1,5 @@
-from src.scraper import get_pag
+from .scraper import get_pag
+from urllib.parse import urljoin
 
 def get_categories(url):
     global categories
@@ -6,12 +7,8 @@ def get_categories(url):
 
     soup = get_pag(url, None)
     
-    def get_Categories(soup):
-        categories = soup.find_all("div", class_="side_categories")
-        return categories
-    
     #extract category names and their URLs
-    for category in get_Categories(soup):
+    for category in  soup.find_all("div", class_="side_categories"):
         category_links = category.find_all("a")
         for link in category_links:
             category_name = link.get_text(strip=True)
@@ -20,3 +17,22 @@ def get_categories(url):
 
     categories.pop(0) #remove the first element which is "Books" category
     return categories
+
+def get_books(url):
+    global books
+    books = []
+
+    while url:#keep looping until there are no more pages (url becomes None)
+        soup = get_pag(url, None)
+
+        for book in soup.find_all("article", class_="product_pod"):
+            book_title = book.h3.a['title']
+            book_url = url + book.h3.a['href']
+            books.append((book_title, book_url))
+
+        next_page_url = soup.find("li", class_="next")
+        if next_page_url: #check if there is a next page
+            url = urljoin(url, next_page_url.a['href'])
+        else:
+            url = None
+    return books
